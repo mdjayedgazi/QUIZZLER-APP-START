963c46aeab922669d4aacd5931491d0ac7381d11
Md.Jayed Gazi <mdjayedgazi@gmail.com>
2025-08-16 17:22:04 +0600

    create a funtion to Question appearance in white canvas

--- ui.py
+++ ui.py
@@ -1,8 +1,10 @@
 from tkinter import*
+from quiz_brain import QuizBrain
 THEME_COLOR = "#375362"
 
 class QuizInterface:
-    def __init__(self):
+    def __init__(self,quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.root = Tk()
         self.root.title("Quizzler")
         self.root.config(padx=20,pady=20,bg=THEME_COLOR)
@@ -11,7 +13,7 @@ class QuizInterface:
         self.wrong_img = PhotoImage(file="images/false.png")
         # Create Canvas
         self.canvas = Canvas(width=300,height=250,bg="white",highlightthickness=0)
-        self.q_text = self.canvas.create_text(150,125,text="Some Quest",width=200,font=("Arial",20,"italic"),fill=THEME_COLOR)
+        self.q_text = self.canvas.create_text(150,125,text="Some Quest",width=270,font=("Arial",20,"italic"),fill=THEME_COLOR)
         self.canvas.grid(row=1, column=0, columnspan=2, pady=20)
         self.score = Label(text=f"Score: {0} ",bg=THEME_COLOR,fg="white",font=("Arial",10,"bold"))
         self.score.grid(row=0,column=1)
@@ -20,4 +22,10 @@ class QuizInterface:
         self.right_button.grid(row=3,column=1,pady=10)
         self.wrong_button = Button(image=self.wrong_img,highlightthickness=0,bd=0)
         self.wrong_button.grid(row=3,column=0,pady=10)
+        self.get_next_question()
+        
         self.root.mainloop()
+        
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.q_text,text = q_text)
