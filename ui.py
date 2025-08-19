f8a13ef0d2300e9db57188fd2437f24d7a6511eb
Md.Jayed Gazi <mdjayedgazi@gmail.com>
2025-08-19 22:27:58 +0600

    create next question funtion

--- ui.py
+++ ui.py
@@ -1,31 +1,41 @@
-from tkinter import*
+import tkinter as tk
+from tkinter import PhotoImage
 from quiz_brain import QuizBrain
+
 THEME_COLOR = "#375362"
 
 class QuizInterface:
-    def __init__(self,quiz_brain: QuizBrain):
+    def __init__(self, quiz_brain: QuizBrain):
         self.quiz = quiz_brain
-        self.root = Tk()
+        self.root = tk.Tk()
         self.root.title("Quizzler")
-        self.root.config(padx=20,pady=20,bg=THEME_COLOR)
+        self.root.config(padx=20, pady=20, bg=THEME_COLOR)
+
         # Load Images 
         self.right_img = PhotoImage(file="images/true.png")
         self.wrong_img = PhotoImage(file="images/false.png")
+
         # Create Canvas
-        self.canvas = Canvas(width=300,height=250,bg="white",highlightthickness=0)
-        self.q_text = self.canvas.create_text(150,125,text="Some Quest",width=270,font=("Arial",20,"italic"),fill=THEME_COLOR)
+        self.canvas = tk.Canvas(width=300, height=250, bg="white", highlightthickness=0)
+        self.q_text = self.canvas.create_text(
+            150, 125, text="Some Quest", width=270,
+            font=("Arial", 20, "italic"), fill=THEME_COLOR
+        )
         self.canvas.grid(row=1, column=0, columnspan=2, pady=20)
-        self.score = Label(text=f"Score: {0} ",bg=THEME_COLOR,fg="white",font=("Arial",10,"bold"))
-        self.score.grid(row=0,column=1)
-        # Button
-        self.right_button = Button(image=self.right_img,highlightthickness=0,bd=0,)
-        self.right_button.grid(row=3,column=1,pady=10)
-        self.wrong_button = Button(image=self.wrong_img,highlightthickness=0,bd=0)
-        self.wrong_button.grid(row=3,column=0,pady=10)
+
+        # Score Label
+        self.score = tk.Label(text=f"Score: 0", bg=THEME_COLOR, fg="white", font=("Arial",10,"bold"))
+        self.score.grid(row=0, column=1)
+
+        # Buttons
+        self.right_button = tk.Button(image=self.right_img, highlightthickness=0, bd=0)
+        self.right_button.grid(row=3, column=1, pady=10)
+        self.wrong_button = tk.Button(image=self.wrong_img, highlightthickness=0, bd=0)
+        self.wrong_button.grid(row=3, column=0, pady=10)
+
         self.get_next_question()
-        
         self.root.mainloop()
-        
+
     def get_next_question(self):
         q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.q_text,text = q_text)
+        self.canvas.itemconfig(self.q_text, text=q_text)
